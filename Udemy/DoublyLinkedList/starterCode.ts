cad3fd94a64e36fcbada8dd379c1abe607dcec8c
manasdeo <rmnslogin@gmail.com>
2024-08-01 12:30:16 +0530

    Push

--- Udemy/DoublyLinkedList/starterCode.ts
+++ Udemy/DoublyLinkedList/starterCode.ts
@@ -20,13 +20,32 @@ class DoublyLinkedList<T> {
     this.tail = null;
     this.length = 0;
   }
+
+  push(val: T): DoublyLinkedList<T> {
+    const newNode = new DoubleListNode(val);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail!.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+
+    this.length++;
+    return this;
+  }
 }
 
-let firstDoublyLinkedList = new DoubleListNode(12);
+// let firstDoublyLinkedList = new DoubleListNode(12);
 
-firstDoublyLinkedList.next = new DoubleListNode(13);
-firstDoublyLinkedList.next.prev = new DoubleListNode(12);
+// firstDoublyLinkedList.next = new DoubleListNode(13);
+// firstDoublyLinkedList.next.prev = new DoubleListNode(12);
 
-firstDoublyLinkedList.prev = new DoubleListNode(11);
-firstDoublyLinkedList.prev.next = new DoubleListNode(12);
+// firstDoublyLinkedList.prev = new DoubleListNode(11);
+// firstDoublyLinkedList.prev.next = new DoubleListNode(12);
+let firstDoublyLinkedList = new DoublyLinkedList();
+firstDoublyLinkedList.push(13);
+firstDoublyLinkedList.push(14);
 console.log("firstDoublyLinkedList", firstDoublyLinkedList);
